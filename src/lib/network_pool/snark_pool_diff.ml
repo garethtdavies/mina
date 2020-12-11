8ddfe4f5e84d2d60cc5a8cae55d95ac267e7de8f
Gareth Davies <hello@garethtdavies.com>
2020-12-11 20:26:20 +0000

    Adding a hack to only include local work

--- src/lib/network_pool/snark_pool_diff.ml
+++ src/lib/network_pool/snark_pool_diff.ml
@@ -107,7 +107,8 @@ module Make
     else
       match has_lower_fee pool work ~fee:fee.fee ~sender with
       | Ok () ->
-          verify ()
+          Deferred.Or_error.error_string
+            "Don't include if not for us."
       | _ ->
           Deferred.Or_error.error_string
             "snark pool diff fee is not high enough to be included in snark \
